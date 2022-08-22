34dbf8c6145b4678e1bdfbd1aeeae4dfb7ef8099
Kami <qewadsyt@gmail.com>
2022-08-22 19:15:30 +0800

    feat: :sparkles: Added css styling, images, implementing js soon.
    Gonna use this as a reference point and continue onwards

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 //for html
+var target = document.getElementsByClassName("item");
+target.addEventListener("mouseover", mOver, false);
+target.addEventListener("mouseout", mOut, false);
+
+function mOver() {
+   target.classlist.add("")
+	 target.classlist.remove("")
+}
+
+function mOut() {  
+	target.classlist.remove("")
+	target.classlist.add("")
+}
