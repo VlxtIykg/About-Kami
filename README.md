6b52c760ff09602424da0a169af6a315bd773ccf
Kami <qewadsyt@gmail.com>
2022-08-26 01:56:10 +0800

    Taking break.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 kami-x.tk
 # Anime website
 aws.kami-x.tk
+
+- Project will be suspended for a little bit to continue on other projects.Should you be interested in helping, Kami#7715 is my discord. Maybe I'll have an official gmail one day.
--- src/index.js
+++ src/index.js
@@ -5,12 +5,20 @@
 // 	eachItem.addEventListener("mouseout", mOut, false);
 // });
 
-function mOver() {
-   target.classlist.add("")
-	 target.classlist.remove("")
+function mOver(manipulationEvent) {
+	if(manipulationEvent === classAdd) {
+   	target.classlist.add("");
+	 	target.classlist.remove("");
+	 }
 }
 
 function mOut() {  
-	target.classlist.remove("")
-	target.classlist.add("")
+	target.classlist.remove("");
+	target.classlist.add("");
 }
+let img = document.querySelector('img');
+let start = img.src;
+let hover = img.getAttribute('data-hover'); //specified in img tag
+
+img.onmouseover = () => { img.src = hover; }
+img.onmouseout = () => { img.src = start; } //to revert back to start
