c73375866cf2605cda71033b222b387dd0341213
Kami <harshimymommy@gmail.com>
2024-01-01 00:27:59 -0800

    ci: 👷 CI stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "http://localhost:4321",
+  integrations: [preact()],
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone"
+  })
+});
