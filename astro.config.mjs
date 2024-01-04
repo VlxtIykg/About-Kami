f6e31c9198f54c9ab15628f1cedaa2fb85841b2b
Kami <harshimymommy@gmail.com>
2024-01-04 10:58:28 +0000

    Fix adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
-import cloudflare from "@astrojs/cloudflare"
 import node from "@astrojs/node";
 
-// https://astro.build/config
 export default defineConfig({
   site: "http://localhost:4321",
   integrations: [preact()],
   output: "hybrid",
-  adapter: cloudflare()
+  adapter: node({
+    mode: "standalone"
+  })
 });
