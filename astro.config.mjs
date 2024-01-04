9b4c15ba824f56e890b8086c0a20d7c22e738ac0
Kami <harshimymommy@gmail.com>
2024-01-04 02:59:29 +0000

    build: 💚 Fix ssr adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
-
+import cloudflare from "@astrojs/cloudflare"
 import node from "@astrojs/node";
 
 // https://astro.build/config
@@ -8,7 +8,5 @@ export default defineConfig({
   site: "http://localhost:4321",
   integrations: [preact()],
   output: "hybrid",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: cloudflare()
 });
