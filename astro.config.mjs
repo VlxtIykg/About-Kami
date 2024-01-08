72d70cf5d4e903dc60b5322d29b019d310aa4d06
Kami <harshimymommy@gmail.com>
2024-01-08 19:41:50 +0000

    build: 💚 Trying to fix no ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   integrations: [preact(), sitemap()],
   output: "hybrid",
   adapter: cloudflare({
-    mode: "directory",
+    mode: "advanced",
     imageService: "cloudflare",
   }),
   server: {
