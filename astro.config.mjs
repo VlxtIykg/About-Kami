9c1df4ce0cb8cead263cf2eb47a4b46d6cb5faad
Kami <harshimymommy@gmail.com>
2024-01-08 23:15:26 +0000

    build: 💚 Packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import node from "@astrojs/node";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://kami-x.tk",
+  site: "http://localhost:4321",
   integrations: [preact(), sitemap()],
   output: "hybrid",
   adapter: node({
