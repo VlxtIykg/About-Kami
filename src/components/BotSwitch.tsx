77e8fbb309e022488a5e1f0b00514d23b63999bd
Kami <harshimymommy@gmail.com>
2024-01-28 06:57:43 -0800

    fix: 🎨 Fix typescript warnings

--- src/components/BotSwitch.tsx
+++ src/components/BotSwitch.tsx
@@ -33,7 +33,7 @@ export default function Form() {
       ws.send("botswitch");
     });
 
-    const response = await fetch("https://api.kami-x.tk/bot_status", {
+    const response = await fetch("http://api.kami-x.tk/bot_status", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
