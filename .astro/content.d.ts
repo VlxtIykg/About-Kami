bacde7ef855949df384c908722fcafd321fa31cf
Kami <97310758+VlxtIykg@users.noreply.github.com>
2025-06-11 11:40:00 +0800

    perf(blog): Updated blog
    Fixed collection warning and added schema typing.

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -151,7 +151,7 @@ declare module 'astro:content' {
   id: string;
   body?: string;
   collection: "posts";
-  data: any;
+  data: InferEntrySchema<"posts">;
   rendered?: RenderedContent;
   filePath?: string;
 }>;
@@ -160,5 +160,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	export type ContentConfig = typeof import("../src/content.config.mjs");
+	export type ContentConfig = typeof import("../src/content.config.js");
 }
