dd5b7cd175b28b5f899055a1bf4a572b95fe5032
Kami <97310758+VlxtIykg@users.noreply.github.com>
2025-06-11 16:17:43 +0800

    fmt(entire project): Single quotes to Double quotes

--- src/scripts/Greeting.jsx
+++ src/scripts/Greeting.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'preact/hooks';
+import { useState } from "preact/hooks";
 
 export default function Greeting({ messages }) {
   const randomMessage = () => messages[Math.floor(Math.random() * messages.length)];
